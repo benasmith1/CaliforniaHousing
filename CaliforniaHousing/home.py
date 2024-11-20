cf5786869fdb925c1ef0aeb03dc7c765b3d51e58
benasmith1 <benapearl@gmail.com>
2024-11-19 17:18:26 -0800

    pages

--- CaliforniaHousing/home.py
+++
@@ -1,9 +0,0 @@
-#Import necessary libraries
-import streamlit as st
-import pickle
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
-st.markdown("hello world")
-
---
+++ CaliforniaHousingApp/home.py
@@ -0,0 +1,9 @@
+#Import necessary libraries
+import streamlit as st
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+st.markdown("hello world")
+
